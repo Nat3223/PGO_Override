b306af1f5b91eb96cf78669b69562043e37388bd
Natalia Suwińska <s33171@pjwstk.edu.pl>
2026-03-31 09:23:47 +0200

    Override

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+        Policy p1 = new Policy("CAR-100", "Jan Nowak", 850.0, 3, 68000.0, true, true);
+        Policy p2 = new Policy("CAR-203", "Anna Kowalska", 920.0, 4, 45000.0, false, false);
+        Policy p3 = new Policy("CAR-230", "Piotr Rafal", 780.0, 2, 52000.0, true, false);
+
+        System.out.println("=== POLICY TEST ===");
+
+        System.out.println("Final premiums:");
+        System.out.println(p1.calculateFinalPremium());
+        System.out.println(p2.calculateFinalPremium());
+        System.out.println(p3.calculateFinalPremium());
+
+        System.out.println();
+
+        System.out.println("Renewal premiums:");
+        System.out.println(p1.calculateRenewalPremium());
+        System.out.println(p2.calculateRenewalPremium());
+        System.out.println(p3.calculateRenewalPremium());
+
+        System.out.println();
+
+        System.out.println("Risk summaries:");
+        System.out.println(p1.getRiskSummary());
+        System.out.println(p2.getRiskSummary());
+
+        System.out.println();
+
+        System.out.println("Equals test:");
+        System.out.println(p1.equals(new Policy("CAR-201", "Test", 700, 1, 30000, false, false)));
+
+        System.out.println();
+
+        System.out.println("toString test:");
+        System.out.println(p1);
+        System.out.println(p2);
+
+        System.out.println();
+
+        System.out.println("Created policies:");
+        System.out.println(Policy.getCreatedPolicyCount());
+
     }
 }
--- src/Policy.java
+++ src/Policy.java
@@ -1,5 +1,6 @@
-public class Policy {
+import java.util.Objects;
 
+public class Policy {
     private String policyNumber;
     private String clientName;
     private double basePremium;
@@ -7,6 +8,108 @@ public class Policy {
     private double vehicleValue;
     private boolean hasAlarm;
     private boolean claimFreeClient;
-    private static int createdPolicyCount;
-    private static final double ADMINISTRATIVE_FEE;
+
+    private static int createdPolicyCount = 0;
+    private static final double ADMINISTRATIVE_FEE = 37.5;
+
+    public Policy(String policyNumber, String clientName, double basePremium, int riskLevel, double vehicleValue,boolean hasAlarm, boolean claimFreeClient) {
+
+        this.policyNumber = policyNumber;
+        this.clientName = clientName;
+        this.basePremium = basePremium;
+        this.riskLevel = riskLevel;
+        this.vehicleValue = vehicleValue;
+        this.hasAlarm = hasAlarm;
+        this.claimFreeClient = claimFreeClient;
+
+        createdPolicyCount++;
+    }
+
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public double calculateFinalPremium() {
+        double premium = basePremium + ADMINISTRATIVE_FEE;
+
+        premium += riskLevel * 95;
+
+        if (vehicleValue > 55000) {
+            premium += 180;
+        }
+
+        if (riskLevel >= 4 && !hasAlarm) {
+            premium += 100;
+        }
+
+        if (hasAlarm) {
+            premium *= 0.93;
+        }
+
+        if (claimFreeClient) {
+            premium *= 0.88;
+        }
+
+        if (premium < basePremium) {
+            premium = basePremium;
+        }
+
+        return Math.round(premium * 100.0) / 100.0;
+
+    }
+
+    public double calculateRenewalPremium() {
+
+        double current = calculateFinalPremium();
+        double renewal = current;
+
+        if (riskLevel == 3) {
+            renewal *= 1.05;
+        } else if (riskLevel == 4) {
+            renewal *= 1.12;
+        } else if (riskLevel >= 5) {
+            renewal *= 1.18;
+        }
+
+        if (vehicleValue > 55000) {
+            renewal += 130;
+        }
+
+        if (claimFreeClient) {
+            renewal *= 0.90;
+        }
+
+        if (hasAlarm) {
+            renewal *= 0.93;
+        }
+
+        double min = current * 0.85;
+        double max = current * 1.30;
+
+        if (renewal < min) renewal = min;
+        if (renewal > max) renewal = max;
+
+        return Math.round(renewal * 100.0) / 100.0;
+    }
+
+    public String getRiskSummary() {
+        return clientName + " (Policy: " + policyNumber + ") Risk level: " + riskLevel;
+    }
+
+    public static int getCreatedPolicyCount() {
+        return createdPolicyCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Policy{" + "number='" + policyNumber + '\'' + ", client='" + clientName + '\'' + ", finalPremium=" + calculateFinalPremium() + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Policy)) return false;
+        Policy policy = (Policy) o;
+        return Objects.equals(policyNumber, policy.policyNumber);
+    }
 }
