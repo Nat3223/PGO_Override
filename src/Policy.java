0daf08038494705176a52dc995a8ab0a87c1e2b3
Natalia Suwińska <s33171@pjwstk.edu.pl>
2026-03-31 09:20:32 +0200

    Override

---
+++ src/Policy.java
@@ -0,0 +1,12 @@
+public class Policy {
+
+    private String policyNumber;
+    private String clientName;
+    private double basePremium;
+    private int riskLevel;
+    private double vehicleValue;
+    private boolean hasAlarm;
+    private boolean claimFreeClient;
+    private static int createdPolicyCount;
+    private static final double ADMINISTRATIVE_FEE;
+}
